3cba1a234a8cd5bb264c113b5a4d6304f40f0502
Jorge Guzman <jorge.indq@gmail.com>
2022-10-19 11:59:39 -0600

    Pa subir

---
+++ csharp-server/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace csharp_server
+{
+
+    public class Echo : WebSocketBehavior
+    {
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            Console.WriteLine("Recibiendo: " + e.Data);
+            Send(e.Data);
+        }
+    }
+
+    public class EchoAll : WebSocketBehavior
+    {
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            Console.WriteLine("Recibiendo All: " + e.Data);
+            Sessions.Broadcast(e.Data);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WebSocketServer server = new WebSocketServer("ws://192.168.1.190:7890");
+
+            server.AddWebSocketService<Echo>("/Echo");
+            server.AddWebSocketService<EchoAll>("/EchoAll");
+
+            server.KeepClean = false;
+            server.Start();
+            Console.WriteLine("Server iniciado: ws://192.168.1.190:7890/Echo");
+            Console.WriteLine("Server iniciado: ws://192.168.1.190:7890/EchoAll");
+
+            Console.ReadKey();
+            //server.Stop();
+        }
+    }
+}
